369f180b1803bc42e3f7007cdfe2a2d62351f3fe
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2023-01-22 17:05:45 +0200

    5 Q&A

--- cpp.cpp
+++ cpp.cpp
@@ -74,4 +74,15 @@ int main()
 	cout << count;
 } 
 
-5)
+5) 50A - Domino piling
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+	int m, n;
+	cin >> m >> n;
+	cout << m * n / 2;
+} 
+
+6) 
