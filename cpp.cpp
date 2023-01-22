e5a8f0bcbc7c2423ecf8e84d4641047a02b5663b
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2023-01-22 11:41:37 +0200

    Create cpp.cpp

---
+++ cpp.cpp
@@ -0,0 +1,16 @@
+1) 4A - Watermelon
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int w;
+	cin >> w;
+	if (w % 2 == 0 && w != 2) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+}
+
+2) 
