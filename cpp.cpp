364f0106fe79f9754149e37592b6c2f627e5acad
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2023-01-22 16:57:12 +0200

    Update cpp.cpp

--- cpp.cpp
+++ cpp.cpp
@@ -51,3 +51,27 @@ int main() {
 	cout << count;
 
 }
+4) Next Round
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	int arr[10000], count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+		if (arr[i] > 0)
+		{
+			if (arr[i] >= arr[k - 1])
+			{
+				count++;
+			}
+		}
+	}
+	cout << count;
+} 
+
+5)
