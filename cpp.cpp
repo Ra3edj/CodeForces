a01985c484b4f2144a82e4a5e756692e0e2ad5e7
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2023-01-27 20:34:28 +0200

    Update cpp.cpp

--- cpp.cpp
+++ cpp.cpp
@@ -200,4 +200,23 @@ int main()
 } 
 
 10) Boy or Girl
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+	string s;
+	vector<int>v;
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		v.push_back(s[i]);
+	}
+	sort(v.begin(), v.end());
+	v.erase(unique(v.begin(), v.end()),v.end());
+	if (v.size() % 2 == 0) {
+		cout << "CHAT WITH HER!";
+	}
+	else {
+		cout << "IGNORE HIM!";
+	}
+} 
