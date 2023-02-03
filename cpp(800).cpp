30025a137e455f753c6c5de053c6c233ac2edb69
Ra3ed Saade <108870616+Ra3edj@users.noreply.github.com>
2023-02-03 13:58:56 +0200

    Update cpp(800).cpp

--- cpp(800).cpp
+++ cpp(800).cpp
@@ -441,5 +441,42 @@ int main()
 	cout << "YES";
 }
 
-21) B. Queue at the School
+21) A. Fafa and his Company
+#include <bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+	double n , counter  = 0;
+	cin >> n;
+	for (int i = 1; i < n; i++) {
+		if (int((n - i) / i) == (n - i) / i) {
+			counter++;
+		}
+	}
+	cout << counter;
+}
+
+22) 1353A - Most Unstable Array	
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	long long t , n ,m;
+	cin >> t;
+	while (t--) {
+		cin >> n >> m;
+		if (n == 1) {
+			cout << 0 << endl;
+		}
+		else if (n == 2) {
+			cout << m << endl;
+		}
+		else {
+			cout << m * 2 << endl;
+		}
+	}
+}
+
+23) 
